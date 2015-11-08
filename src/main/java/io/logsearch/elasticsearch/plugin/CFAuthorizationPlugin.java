b2f68ebe8e716981002cd290fd4ac82555f381dd
David Laing <david@davidlaing.com>
2015-11-08 15:11:18 +0000

    Basic plugin skeleton

---
+++ src/main/java/io/logsearch/elasticsearch/plugin/CFAuthorizationPlugin.java
@@ -0,0 +1,70 @@
+package io.logsearch.elasticsearch.plugin;
+
+import org.elasticsearch.common.component.LifecycleComponent;
+import org.elasticsearch.common.inject.AbstractModule;
+import org.elasticsearch.common.inject.Module;
+import org.elasticsearch.common.inject.multibindings.Multibinder;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.repositories.RepositoriesModule;
+import org.elasticsearch.rest.action.cat.AbstractCatAction;
+
+import java.io.Closeable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class CFAuthorizationPlugin extends Plugin {
+
+    private final Settings settings;
+
+    public CFAuthorizationPlugin(Settings settings) {
+        this.settings = settings;
+    }
+
+    @Override
+    public String name() {
+        return "cf-authorization plugin";
+    }
+
+    @Override
+    public String description() {
+        return "An Elasticsearch plugin that restricts access to log documents based on CF UAA credentials";
+    }
+
+    @Override
+    public Collection<Module> nodeModules() {
+        return Collections.<Module>singletonList(new ConfiguredExampleModule());
+    }
+
+    @Override
+    public Collection<Class<? extends LifecycleComponent>> nodeServices() {
+        Collection<Class<? extends LifecycleComponent>> services = new ArrayList<>();
+        return services;
+    }
+
+    @Override
+    public Settings additionalSettings() {
+        return Settings.EMPTY;
+    }
+
+    // public void onModule(ActionModule actionModule) {
+    //     actionModule.registerFilter(DLSActionFilter.class);
+    // }
+
+    public void onModule(RepositoriesModule repositoriesModule) {
+    }
+
+    /**
+     * Module decalaring some example configuration and a _cat action that uses
+     * it.
+     */
+    public static class ConfiguredExampleModule extends AbstractModule {
+        @Override
+        protected void configure() {
+          bind(CFAuthorizationPluginConfiguration.class).asEagerSingleton();
+          Multibinder<AbstractCatAction> catActionMultibinder = Multibinder.newSetBinder(binder(), AbstractCatAction.class);
+          catActionMultibinder.addBinding().to(CFAuthorizationCatAction.class).asEagerSingleton();
+        }
+    }
+}
