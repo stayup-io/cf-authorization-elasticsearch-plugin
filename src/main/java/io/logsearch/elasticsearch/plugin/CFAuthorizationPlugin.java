c790b6315e7f353ebdcd73c5803c3f39dec179b3
David Laing <david@davidlaing.com>
2015-11-12 15:51:51 +0000

    Remove unused config

--- src/main/java/io/logsearch/elasticsearch/plugin/CFAuthorizationPlugin.java
+++ src/main/java/io/logsearch/elasticsearch/plugin/CFAuthorizationPlugin.java
@@ -33,39 +33,11 @@ public class CFAuthorizationPlugin extends Plugin {
         return "An Elasticsearch plugin that restricts access to log documents based on CF UAA credentials";
     }
 
-    @Override
-    public Collection<Module> nodeModules() {
-        return Collections.<Module>singletonList(new ConfiguredExampleModule());
-    }
-
-    @Override
-    public Collection<Class<? extends LifecycleComponent>> nodeServices() {
-        Collection<Class<? extends LifecycleComponent>> services = new ArrayList<>();
-        return services;
-    }
-
-    @Override
-    public Settings additionalSettings() {;
-        return Settings.EMPTY;
-    }
-
-     public void onModule(ActionModule actionModule) {
+    public void onModule(ActionModule actionModule) {
          actionModule.registerFilter(CFAuhorizationActionFilter.class);
      }
 
     public void onModule(RepositoriesModule repositoriesModule) {
     }
 
-    /**
-     * Module declaring some example configuration and a _cat action that uses
-     * it.
-     */
-    public static class ConfiguredExampleModule extends AbstractModule {
-        @Override
-        protected void configure() {
-          bind(CFAuthorizationPluginConfiguration.class).asEagerSingleton();
-          Multibinder<AbstractCatAction> catActionMultibinder = Multibinder.newSetBinder(binder(), AbstractCatAction.class);
-          catActionMultibinder.addBinding().to(CFAuthorizationCatAction.class).asEagerSingleton();
-        }
-    }
 }
